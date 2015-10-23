6cd79633dfa63990aea384f02f95cf9afb3494d0
Justin <gladiator.xu@outlook.com>
2015-10-23 15:19:43 +0800

    a test file for delete

---
+++ PolDemo/src/com/gladiatorxu/PolDemo/PolDemo04.java
@@ -0,0 +1,25 @@
+package com.gladiatorxu.PolDemo;
+
+public class PolDemo04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * 
+		 *
+		 */
+		System.out.println("just test git");
+	}
+	/**
+	 * 
+	 * @param i
+	 * @param j
+	 */
+	public static void tell(int i,String j){
+		System.out.println("another test");
+	}
+
+}
